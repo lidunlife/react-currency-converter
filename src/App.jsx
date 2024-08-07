594fce75b8e48ce16abdab3660f4be314f37935e
lidunlife <uchihalidun@gmail.com>
2024-08-07 16:34:05 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ConverterForm from './components/convertorForm';
+
+function App() {
+  return (
+
+    <div className='Currency-converter'>
+      <h2 className="cunverter-title">Currency Converter</h2>
+    <ConverterForm></ConverterForm>
+    </div>
+
+
+  );
+}
+
+export default App;
